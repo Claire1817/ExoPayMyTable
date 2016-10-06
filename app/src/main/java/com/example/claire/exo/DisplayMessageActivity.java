7bbefcfcd59a4fa0d241b286919b266df5b3625d
ClaireGizard <claire.1817@hotmail.fr>
2016-10-06 17:52:34 +0200

    Add Spinner (loading image)

--- app/src/main/java/com/example/claire/exo/DisplayMessageActivity.java
+++ app/src/main/java/com/example/claire/exo/DisplayMessageActivity.java
@@ -2,15 +2,17 @@ package com.example.claire.exo;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
-
 import java.util.HashMap;
 import java.util.List;
-
 import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
@@ -23,7 +25,6 @@ public class DisplayMessageActivity extends AppCompatActivity {
     private RetrofitManager tmp;
 
     private String message;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,6 +72,8 @@ public class DisplayMessageActivity extends AppCompatActivity {
 
         TypedByteArray ret = (TypedByteArray)response.getBody();
         Bitmap decodeByte = BitmapFactory.decodeByteArray(ret.getBytes(), 0, ret.getBytes().length);
+        ProgressBar spinner = (ProgressBar)findViewById(R.id.progressBar1);
+        spinner.setVisibility(View.GONE);
         ImageView image = (ImageView)findViewById(R.id.image_text);
         image.setImageBitmap(decodeByte);
     }
